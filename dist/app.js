23925aa832878a69fe81fcb2532b067cd70e89a0
Axuby <azubuinesamuel@gmail.com>
2021-08-10 21:12:06 +0100

    updated css file

--- dist/app.js
+++ dist/app.js
@@ -6,3 +6,5 @@ class Project {
         const importNode = document.importNode(this.templateElement.content, true);
     }
 }
+const p1 = new Project();
+console.log(p1);
--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ class Project {
     hostElement: HTMLDivElement;
 
     constructor() {
-        this.templateElement = <HTMLTemplateElement> document.getElementById('project-input')!
-        this.hostElement =  <HTMLDivElement> document.getElementById('app')!
-   
-        const importNode = document.importNode(this.templateElement.content,true)
+        this.templateElement = <HTMLTemplateElement> document.getElementById('project-input')!;
+        this.hostElement =  <HTMLDivElement> document.getElementById('app')!;
+         const importNode = document.importNode(this.templateElement.content,true)
     }
-
-
-    
+   
 }
+
+
+
+const p1 = new Project();
+console.log(p1);
+
