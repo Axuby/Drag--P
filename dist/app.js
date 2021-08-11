891e5853d01964d06b5de75d80a9d0d58b896666
Axuby <azubuinesamuel@gmail.com>
2021-08-11 20:11:03 +0100

    OOP implementation

--- dist/app.js
+++ dist/app.js
@@ -3,7 +3,13 @@ class Project {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
-        const importNode = document.importNode(this.templateElement.content, true);
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
 const p1 = new Project();
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,35 @@
 class Project {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputElement : HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement
+
+ 
 
     constructor() {
         this.templateElement = <HTMLTemplateElement> document.getElementById('project-input')!;
         this.hostElement =  <HTMLDivElement> document.getElementById('app')!;
-         const importNode = document.importNode(this.templateElement.content,true)
+
+    
+         const importedNode = document.importNode(this.templateElement.content,true)
+         this.element = <HTMLFormElement> importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputElement = <HTMLInputElement> document.querySelector('#title');
+       
+         this.attach();
     }
-   
+
+
+        attach(){
+            this.hostElement.insertAdjacentElement('afterbegin',this.element)
+        }
 }
 
 
 
 const p1 = new Project();
 console.log(p1);
+
 
