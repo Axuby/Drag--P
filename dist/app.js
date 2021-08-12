519a20b12b92e260cde9f94f56ee15c6e2f8f2d8
Axuby <azubuinesamuel@gmail.com>
2021-08-12 20:25:13 +0100

    created class methods

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,17 @@ class Project {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
         this.element.id = 'user-input';
+        this.titleInputElement = document.querySelector('#title');
+        this.descriptionInputElement = document.querySelector('#description');
+        this.peopleInputElement = document.querySelector('#people');
+        this.configure();
         this.attach();
+    }
+    submitHandler(event) {
+        event.preventDefault();
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 class Project {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -12,17 +19,33 @@ class Project {
         this.templateElement = <HTMLTemplateElement> document.getElementById('project-input')!;
         this.hostElement =  <HTMLDivElement> document.getElementById('app')!;
 
-    
          const importedNode = document.importNode(this.templateElement.content,true)
+
          this.element = <HTMLFormElement> importedNode.firstElementChild;
         this.element.id = 'user-input';
+
         this.titleInputElement = <HTMLInputElement> document.querySelector('#title');
-       
+        this.descriptionInputElement =<HTMLInputElement> document.querySelector('#description')
+        this.peopleInputElement =<HTMLInputElement> document.querySelector('#people')
+        
+
+        this.configure()
          this.attach();
     }
 
 
-        attach(){
+
+  
+      private submitHandler(event:Event){
+        event.preventDefault(); 
+
+      } 
+
+        private configure() {
+            this.element.addEventListener('submit',this.submitHandler.bind(this))
+        }
+
+       private attach(){
             this.hostElement.insertAdjacentElement('afterbegin',this.element)
         }
 }
