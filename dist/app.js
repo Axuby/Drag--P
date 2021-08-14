5e78c1548e310d60619a7bef788ddc7a08564f56
Axuby <azubuinesamuel@gmail.com>
2021-08-14 15:55:30 +0100

    implemented form logic

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,22 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function AutoBind(target, methodName, descriptor) {
+    const boundValue = descriptor.value;
+    const adjDescriptor = {
+        enumerable: true,
+        configurable: true,
+        get() {
+            let realValue = boundValue.bind(this);
+            return realValue;
+        }
+    };
+    return adjDescriptor;
+}
 class Project {
     constructor() {
         this.templateElement = document.getElementById('project-input');
@@ -12,15 +30,38 @@ class Project {
         this.configure();
         this.attach();
     }
+    gatherUserInput() {
+        const titleValue = this.titleInputElement.value;
+        const descriptionValue = this.descriptionInputElement.value;
+        const peopleValue = this.peopleInputElement.value;
+        if (titleValue.trim().length === 0 ||
+            descriptionValue.trim().length === 0 || peopleValue.trim().length === 0) {
+            alert('Please input a value ');
+        }
+        else {
+            return [titleValue, descriptionValue, +peopleValue];
+        }
+    }
+    clearInput() {
+        this.descriptionInputElement.value = '';
+        this.titleInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
     submitHandler(event) {
         event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [titleValue, descriptionValue, peopleValue] = userInput;
+            console.log(titleValue, descriptionValue, +peopleValue);
+        }
     }
     configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
-const p1 = new Project();
-console.log(p1);
+__decorate([
+    AutoBind
+], Project.prototype, "submitHandler", null);
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
+function AutoBind(target:any, methodName: string, descriptor:PropertyDescriptor) {
+  const boundValue = descriptor.value
 
 
+  const adjDescriptor = {
+    enumerable:true,
+    configurable:true,
+    get(){
+      let realValue = boundValue.bind(this)
+      return realValue
+    }
+  } 
+  return adjDescriptor
+    
+}
 
 
-
-
-
+ 
 class Project {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
@@ -33,16 +44,43 @@ class Project {
          this.attach();
     }
 
-
-
   
-      private submitHandler(event:Event){
-        event.preventDefault(); 
 
+    private gatherUserInput(): [string, string,number]|void {
+      const titleValue = this.titleInputElement.value;
+      const descriptionValue= this.descriptionInputElement.value;
+      const peopleValue = this.peopleInputElement.value;
+
+      if (titleValue.trim().length === 0 ||
+       descriptionValue.trim().length ===0 ||  peopleValue.trim().length ===0) {
+      alert('Please input a value ')
+      } else {
+        return [titleValue,descriptionValue, +peopleValue]
+      }
+    }
+private clearInput(){
+  this.descriptionInputElement.value = '';
+  this.titleInputElement.value = '';
+  this.peopleInputElement.value = '';
+}
+
+
+
+    @AutoBind
+      private submitHandler(event:Event){
+            event.preventDefault(); 
+        const userInput = this.gatherUserInput()
+        if (Array.isArray(userInput)) {
+          const [titleValue,descriptionValue, peopleValue] = userInput
+
+          console.log(titleValue,descriptionValue, +peopleValue)
+        }
+        
+    
       } 
 
         private configure() {
-            this.element.addEventListener('submit',this.submitHandler.bind(this))
+            this.element.addEventListener('submit',this.submitHandler)
         }
 
        private attach(){
@@ -52,7 +90,6 @@ class Project {
 
 
 
-const p1 = new Project();
-console.log(p1);
+
 
 
